d18f14448ddb9a2772521b050e929f107599c637
VitorHAlves <vhssantos2005@gmail.com>
2026-02-23 22:15:01 -0300

    Introdução a Classe e POO com exemplos

---
+++ ClasseConta/Conta.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClasseConta
+{
+    public class Conta
+    {
+        //declaração de atributos
+        public int numero;
+        public string titular;
+        public double saldo;
+
+        //declaração de funções/métodos
+        public void Sacar(double valor)
+        {
+            saldo -= valor;
+        }
+        public void Depositar()
+        {
+
+        }
+        public void MostrarAtributos()
+        {
+         Console.WriteLine($"Numero: {numero} \tTitular: {titular} \tSaldo: {saldo}");   
+        }
+    }
+}
---
+++ ClasseConta/Program.cs
@@ -0,0 +1,16 @@
+﻿using ClasseConta;// sem isso a main n usa a classe conta, e no arquivo conta ela deve estar public.
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Conta c1 = new Conta();//Instância de objeto
+        c1.numero = 1;
+        c1.titular = "Fatec";
+        c1.saldo = 100;
+        c1.MostrarAtributos();
+        c1.Sacar(50);   
+        c1.MostrarAtributos();
+        Conta c2 = new Conta();
+        c2.MostrarAtributos();
+    }
+}
