18e01bcaaaa3f5e82f44618d33f1a31e88e75d33
Vitor Hugo <vhssantos2005@gmail.com>
2026-03-12 07:42:38 -0300

    Estruturação do exercicio da ClasseEstudante concluída

--- ClasseEstudante/Estudante.cs
+++ ClasseEstudante/Estudante.cs
@@ -7,8 +7,61 @@ namespace ClasseEstudante
 {
     public class Estudante
     {
-        private string nome;
-        private int nota;
-        
+        private string? nome;
+        private double nota;
+
+        public double Nota
+        {
+            set
+            {
+                if (value >= 0 && value <= 10)
+                    this.nota = value;
+                else
+                    Console.WriteLine("Nota inválida informada.");
+            }
+            get
+            {
+                return this.nota;
+            }
+        }
+        public string Nome
+        {
+            set
+            {
+                this.nome = value;
+            }
+            get
+            {
+                return this.nome.ToUpper();
+            }
+        }
+
+        public bool notaValida()
+        {
+            if(nota > 0 && nota <= 10)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        public void MostrarAtributos()
+        {
+         Console.WriteLine($"Nome: {this.Nome} \tNota: {this.Nota}");   
+        }
+        public bool EstaAprovado()
+        {
+            if (nota >= 6)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        public void ExibirInformações()
+        {
+            bool situacao = EstaAprovado();
+            Console.WriteLine("Status: "+ (EstaAprovado() ? "Aprovado": "Reprovado"));
+        }
     }
 }
--- ClasseEstudante/Program.cs
+++ ClasseEstudante/Program.cs
@@ -1,8 +1,30 @@
-﻿using ClasseEstudante;
+﻿using System.Data;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Globalization;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime;
+using ClasseEstudante;
 internal class Program
 {
     private static void Main(string[] args)
     {
+        Estudante aluno1 = new Estudante();
+        Console.Write("Digite o nome do aluno: ");
+        aluno1.Nome = Console.ReadLine();
+        Console.Write("Digite a nota do aluno: ");
+        aluno1.Nota = Convert.ToDouble(Console.ReadLine());
+        aluno1.MostrarAtributos();
+        aluno1.ExibirInformações();
 
+        Estudante aluno2 = new Estudante();
+        Console.Write("Digite o nome do aluno: ");
+        aluno2.Nome = Console.ReadLine();
+        Console.Write("Digite a nota do aluno: ");
+        aluno2.Nota = Convert.ToDouble(Console.ReadLine());
+        aluno2.MostrarAtributos();
+        aluno2.ExibirInformações();
+        
     }
 }
